a78023e8b44549073d496b29b96f2e79e4357ae3
“AdityaRaj150” <adityaraj10544@gmail.com>
2025-01-19 20:06:52 +0530

    imported images added game screen

--- client/src/screens/game.tsx
+++ client/src/screens/game.tsx
@@ -1,6 +1,7 @@
+import { PlayerCharacter } from "../components/player";
 
 export function GameScreen(){
     return(<div className="w-screen h-screen bg-green-300" >
-        
+        <PlayerCharacter />
     </div>)
 }
